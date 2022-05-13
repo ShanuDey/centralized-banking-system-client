6bc99206d7c46c43534b784784333de3be44ed72
ShanuDey <sd4shanudey@gmail.com>
2022-05-13 22:01:07 +0530

    defining routes
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route, Link } from "react-router-dom";
 import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
-      <Login />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+      </Routes>
     </>
   );
 }
