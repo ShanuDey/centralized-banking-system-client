113ae6ff16bc6f5cda67e2bf438824a97172d55b
ShanuDey <sd4shanudey@gmail.com>
2022-05-13 22:02:11 +0530

    changed nav bar links with react router's route
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#">CBS</Navbar.Brand>
+        <Navbar.Brand href="/">CBS</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav>
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Login</Nav.Link>
-            <Nav.Link href="#action2">Sign Up</Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/login">
+              Login
+            </Nav.Link>
+            <Nav.Link as={Link} to="/signup">
+              Sign Up
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
