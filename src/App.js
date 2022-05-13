caab769cc0f42a8cd9dade4ff4f087db515290cd
ShanuDey <sd4shanudey@gmail.com>
2022-05-13 21:46:57 +0530

    testing barebone with layout and login page
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div>Hello World</div>
+    <>
+      <Login />
+    </>
   );
 }
 
