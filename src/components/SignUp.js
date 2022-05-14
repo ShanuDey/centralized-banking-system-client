72cd4bcde475f8386e54e6940f6d8826147a9d97
ShanuDey <sd4shanudey@gmail.com>
2022-05-14 11:32:36 +0530

    implemented registration
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,47 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "./Layout";
 import { Card, Form, Button, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import Login from "./Login";
 
 const SignUp = () => {
+  const [firstName, setFirstName] = useState("");
+  const [middleName, setMiddleName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [contact, setContact] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:8000/register", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName,
+        middleName,
+        lastName,
+        email,
+        contact,
+        password,
+      }),
+    })
+      .then(function (response) {
+        console.log(response);
+        alert("Registered successfully");
+      })
+      .catch(function (error) {
+        console.log(error);
+        alert("Registration failed");
+      });
+  };
+
   return (
     <Layout>
       <div className="container">
         <Card className="m-5">
           <Card.Header className="text-center">Registration</Card.Header>
           <Card.Body>
-            <Form>
+            <Form onSubmit={handleSubmit}>
               <Row className="mb-3">
                 <Form.Group as={Col} controlId="formGridFirstName">
                   <Form.Label>First Name</Form.Label>
-                  <Form.Control type="email" placeholder="First Name" />
+                  <Form.Control
+                    type="text"
+                    placeholder="First Name"
+                    name="firstName"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridMiddleName">
                   <Form.Label>Middle Name</Form.Label>
-                  <Form.Control type="email" placeholder="Middle Name" />
+                  <Form.Control
+                    type="text"
+                    placeholder="Middle Name"
+                    name="middleName"
+                    value={middleName}
+                    onChange={(e) => setMiddleName(e.target.value)}
+                  />
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridLastName">
                   <Form.Label>Last Name</Form.Label>
-                  <Form.Control type="email" placeholder="Last Name" />
+                  <Form.Control
+                    type="text"
+                    placeholder="Last Name"
+                    name="lastName"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
                 </Form.Group>
               </Row>
 
               <Form.Group className="mb-3" controlId="formGridEmail">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Email" />
+                <Form.Control
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formGridContact">
                 <Form.Label>Contact</Form.Label>
-                <Form.Control placeholder="Contact" />
+                <Form.Control
+                  type="number"
+                  placeholder="Contact"
+                  name="contact"
+                  value={contact}
+                  onChange={(e) => setContact(e.target.value)}
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formGridPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </Form.Group>
 
               <Button variant="primary" type="submit">
