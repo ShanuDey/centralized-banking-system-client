beb630f799203ab64727b595991e684af7d8d778
ShanuDey <sd4shanudey@gmail.com>
2022-05-14 09:35:55 +0530

    design login form
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import Layout from './Layout'
+import React from "react";
+import Layout from "./Layout";
+import { Card, Button, Form } from "react-bootstrap";
 
 const Login = () => {
-    return (
-        <Layout>
-            Login
-        </Layout>
-    )
-}
+  return (
+    <Layout>
+      <div className="container">
+        <Card className="m-5">
+          <Card.Header className="text-center">Login</Card.Header>
+          <Card.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
 
-export default Login
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Button variant="success" type="submit">
+                Login
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+      </div>
+    </Layout>
+  );
+};
+
+export default Login;
