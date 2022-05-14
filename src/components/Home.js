1b3debdce317ae180ca669b5fba92437bb2ffb9b
ShanuDey <sd4shanudey@gmail.com>
2022-05-14 10:57:03 +0530

    update home page
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Layout from "./Layout";
+import { Card, Button } from "react-bootstrap";
 
 const Home = () => {
   return (
     <Layout>
-      <div>Home</div>
+      <div className="container mt-5">
+        <Card className="text-center">
+          <Card.Header>CBS</Card.Header>
+          <Card.Body>
+            <Card.Title>Centralized Banking System</Card.Title>
+            <Card.Text>For you all banking related service</Card.Text>
+            <Button variant="primary">Start Banking</Button>
+          </Card.Body>
+          <Card.Footer className="text-muted">Secured</Card.Footer>
+        </Card>
+      </div>
     </Layout>
   );
 };
